adc5395569d1bd3f6bf943e7e6cf1110c21dcdb8
Almira Sözlü <asozlu@Almira-Air.station>
2022-05-01 19:34:32 +0200

    ..

--- Fitbase_Data.R
+++ Fitbase_Data.R
@@ -4,13 +4,13 @@ library(tidyverse)
 library(janitor)
 
 # Loading the data -----
-daily_activity <- read_csv(".../Google Data Analytics/Fitabase_Data/dailyActivity_merged.csv")
-heart_rate <- read_csv(".../Google Data Analytics/Fitabase_Data/heartrate_seconds_merged.csv")
-hourly_calories <- read_csv(".../Google Data Analytics/Fitabase_Data/hourlyCalories_merged.csv")
-hourly_intensities <- read_csv(".../Desktop/Google Data Analytics/Fitabase_Data/hourlyIntensities_merged.csv")
-hourly_steps <- read_csv(".../Google Data Analytics/Fitabase_Data/hourlySteps_merged.csv")
-daily_sleep <- read_csv(".../Google Data Analytics/Fitabase_Data/sleepDay_merged.csv")
-weight_log <- read_csv(".../Google Data Analytics/Fitabase_Data/weightLogInfo_merged.csv")
+daily_activity <- read_csv(".../Desktop/Google Data Analytics/Fitabase_Data/dailyActivity_merged.csv")
+heart_rate <- read_csv(".../Desktop/Google Data Analytics/Fitabase_Data/heartrate_seconds_merged.csv")
+hourly_calories <- read_csv(".../Desktop/Google Data Analytics/Fitabase_Data/hourlyCalories_merged.csv")
+hourly_intensities <- read_csv(".../Desktop/Desktop/Google Data Analytics/Fitabase_Data/hourlyIntensities_merged.csv")
+hourly_steps <- read_csv(".../Desktop/Google Data Analytics/Fitabase_Data/hourlySteps_merged.csv")
+daily_sleep <- read_csv(".../Desktop/Google Data Analytics/Fitabase_Data/sleepDay_merged.csv")
+weight_log <- read_csv(".../Desktop/Google Data Analytics/Fitabase_Data/weightLogInfo_merged.csv")
 
 head(daily_activity) #date - char
 head(heart_rate) #datetime - char
@@ -29,4 +29,23 @@ n_distinct(hourly_steps$Id) #33
 n_distinct(daily_sleep$Id) #24
 n_distinct(weight_log$Id) #8 
 
+# Data Cleaning -----
+daily_activity %>% 
+  filter(TotalSteps == 0)
+# 77 entries among 940 have Total Step value of 0
+daily_activity <- daily_activity %>% 
+  filter(TotalSteps != 0)
+
+str(hourly_intensities)
+hourly_intensities %>% 
+  filter(TotalIntensity == 0)
+#9.087 entries among 22.099 have Total Intensity value of 0
+hourly_intensities <- hourly_intensities %>% 
+  filter(TotalIntensity != 0)
+
+hourly_steps %>% 
+  filter(StepTotal == 0)
+# 9287 entries among 22.098 have Total Step value of 0
+hourly_steps <- hourly_steps %>% 
+  filter(StepTotal != 0)
 
